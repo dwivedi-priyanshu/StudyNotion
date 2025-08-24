77f507db80a97ffc34f8bda7ec33f40e522f8658
dwivedi-priyanshu <priyanshudwivedi435@gmail.com>
2025-08-24 21:00:51 +0530

    Fixed db issue

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");   // ⬅️ this line was missing
 require("dotenv").config();
 
 exports.connect = () => {
-  mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log("Db connection established"))
   .catch((err) => {
     console.log("Db connection Failed");
